b1eb197a53744db1f08ffd4d159ff22b240b8954
junyeon1997 <junyeon2012@gmail.com>
2021-07-21 14:40:03 +0900

    프로그래머스 방문길이(구현)

---
+++ programmers/Main12.py
@@ -0,0 +1,18 @@
+def solution(dirs):
+    answer = 0
+    x,y=0,0
+    visit=set()
+    for d in dirs:
+        if d=='U' and y<5:
+            visit.add(((x,y),(x,y+1)))
+            y+=1
+        elif d=='D'and y>-5:
+            visit.add(((x,y-1),(x,y)))
+            y-=1
+        elif d=='L' and x>-5:
+            visit.add(((x-1,y),(x,y)))
+            x-=1
+        elif d=='R' and x<5:
+            visit.add(((x,y),(x+1,y)))
+            x+=1   
+    return len(visit)
