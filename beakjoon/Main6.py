005870d9d58924e89da8d246140d178dd35a1b9f
junyeon1997 <junyeon2012@gmail.com>
2021-07-30 12:36:03 +0900

    백준 온라인저지 1476번(구현, 부르트 포스 알고리즘)

---
+++ beakjoon/Main6.py
@@ -0,0 +1,16 @@
+E, S, M = map(int, input().split())
+e, s, m, cnt = 1, 1, 1, 1
+while True:
+    if e == E and s == S and m == M:
+        break
+    e += 1
+    s += 1
+    m += 1
+    cnt += 1
+    if e >= 16:
+        e = 1
+    if s >= 29:
+        s = 1
+    if m >= 20:
+        m = 1
+print(cnt)
